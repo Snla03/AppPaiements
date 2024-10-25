eabec9f977f6f4160764398fd67eadffb97d016b
achille loic <300134022@monboreal.ca>
2024-10-25 07:28:39 +0200

    creation de la classe abstraite mere paiement

--- AppPaiement/AppPaiement/Program.cs
+++ AppPaiement/AppPaiement/Program.cs
@@ -4,4 +4,20 @@
     {
         Console.WriteLine("Hello, World!");
     }
+    public abstract class paiement
+    {
+
+        public abstract string Description { get; set; }
+        public abstract int Montant { get; set; }
+
+        public paiement(string description, int montant)
+        {
+            Description = description;
+            Montant = montant;
+        }
+        public void AfficherDetails()
+        {
+            Console.WriteLine($"Description: {Description}, Montant: {Montant}");
+        }
+     }
 }
