0ac51db0f2da8060d7f0fc9516ae20d54add31a0
achille loic <300134022@monboreal.ca>
2024-10-26 07:16:35 +0200

    creation de la classe  utilisateur

---
+++ AppPaiement/AppPaiement/Utilisateur.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiement
+{
+    internal class Utilisateur
+    {
+        public string Nom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+        public Utilisateur(string nom)
+        {
+            Nom = nom;
+            Paiements = new List<Paiement>();
+        }
+
+        public void AfficherInfos()
+        {
+            Console.WriteLine($"Utilisateur : {Nom}");
+            Console.WriteLine("Liste des paiements :");
+            foreach (var paiement in Paiements)
+            {
+                paiement.AfficherDetails();
+         
+            }
+            void AjouterPaiement(Paiement paiement)
+            {
+                Paiements.Add(paiement);
+            }
+           
+              
+            
+        }
+
+    }
+}
